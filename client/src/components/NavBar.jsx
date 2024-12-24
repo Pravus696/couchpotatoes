12b58a0b062a2c92bd1eb1675a99ff90ea8a828d
Kodyzz1 <kodyeandrews@gmail.com>
2024-12-23 20:03:16 -0500

    fixed import statements across all files to be complient with directory changes

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/NavBar.css';
+
+const NavBar = () => {
+  return (
+    <header className="navbar">
+      <div className="navbar-brand">
+        <img src="/path-to-your-logo.png" alt="Logo" className="navbar-logo" />
+        <h1 className="navbar-title">CouchPotatoes</h1>
+      </div>
+      <nav>
+        <ul className="navbar-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><Link to="/messages">Messages</Link></li>
+          <li><Link to="/notifications">Notifications</Link></li>
+          <li><Link to="/settings">Settings</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Card from '../assets/styles/Card.css';
+import '../assets/styles/Card.css';
 import '../assets/styles/Home.css';
 
 const Home = () => {
