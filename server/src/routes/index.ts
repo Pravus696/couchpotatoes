7e6326d0b7ab8de20eeeecb525c03404c3455178
Keegan Hinton <khinton386@gmail.com>
2024-12-23 19:20:43 -0500

    wrapped api in router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,7 +6,9 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-import apiRoutes from './api/imageUploadAPI.js';
+
+// Import the updated image upload API route
+import apiRoutes from './api/imageUploadAPI';
 
 router.use('/api', apiRoutes);
 
